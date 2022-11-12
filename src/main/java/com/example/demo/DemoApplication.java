b633963f8da9f240102ec68855b19b98e3803220
Vidish2581 <105219608+Vidish2581@users.noreply.github.com>
2022-11-12 13:59:56 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoApplication {
 
 	
-	  @Value("${NAME:SleepCoutnry World}")
+	  @Value("${NAME:SleepCoutnry}")
 	  String name;
 
 	  @RestController
